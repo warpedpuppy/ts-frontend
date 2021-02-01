e5b6efab9a1b2d594842ecd8339c6b708c1efd3e
ted <ted@warpedpuppy.com>
2021-02-01 15:53:55 -0500

    rgb

--- src/pages/css/color/rgb/RBGSlider.js
+++ src/pages/css/color/rgb/RBGSlider.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function RBGSlider(props) {
     return (
-        <select onChange={(e) => props.changeColor(props.id, e.target.value)}>
+        <select value={props.color} onChange={(e) => props.changeColor(props.id, e.target.value)}>
         {
             props.arr.map( (item, index) => {
-                return <option key={index}>{item}</option>
+                let selected = item === props.color ? true : false;
+                return <option value={item} key={index}>{item}</option>
             })
         }
         </select>
--- src/pages/css/color/rgb/RGB.js
+++ src/pages/css/color/rgb/RGB.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import './RGB.css'
 import RBGSlider from './RBGSlider';
 export default class RGB extends Component {
-  state = {r: 0, g: 0, b: 0}
+  state = {r:0, g: 0, b: 0}
   arr = [...Array(251).keys()]
+  componentDidMount = () => {
+      this.setState({r: this.random(), g: this.random(), b: this.random()})
+  }
   changeColor = (id, val) => {
       console.log(val, id)
       let newObj = Object.assign(this.state)
       newObj[id] = parseInt(val, 10)
      this.setState(newObj, () => console.log(this.state))
      // this.setState({rgb: })
+  }
+  random = () =>{
+      return Math.floor(Math.random() * 250)
   }
   render() {
     let {r,g,b} = this.state;
@@ -18,9 +24,9 @@ export default class RGB extends Component {
     }
     return (
       <div> RGB 
-          <RBGSlider arr={this.arr} id="r" changeColor={this.changeColor} />
-          <RBGSlider arr={this.arr} id="g" changeColor={this.changeColor} />
-          <RBGSlider arr={this.arr} id="b" changeColor={this.changeColor} />
+          <RBGSlider arr={this.arr} id="r" color={r} changeColor={this.changeColor} />
+          <RBGSlider arr={this.arr} id="g" color={g} changeColor={this.changeColor} />
+          <RBGSlider arr={this.arr} id="b" color={b} changeColor={this.changeColor} />
           <div className="rgb-product" style={style}></div>
       </div>
     );
