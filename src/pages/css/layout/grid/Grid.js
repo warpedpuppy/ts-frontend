5babe5ac5d0fab807c05d60b354bdb9be37777ce
ted <ted@warpedpuppy.com>
2021-02-25 08:09:59 -0500

    set up code for grid rows and cols

--- src/pages/css/layout/grid/Grid.js
+++ src/pages/css/layout/grid/Grid.js
@@ -2,18 +2,80 @@ import React, { Component } from 'react';
 import './Grid.css';
 import {Form, InputGroup, Table, FormControl} from 'react-bootstrap';
 export default class Grid extends Component {
-  state = {gridTemplateColumns: "1fr 1fr 1fr", gridTemplateRows: "1fr 1fr 1fr", category: "gridContainer", activeGridItem: 1}
+  state = {
+    gridTemplateColumns: "1fr 1fr 1fr", 
+    gridTemplateRows: "1fr 1fr 1fr", 
+    category: "gridItems", 
+    activeGridItem: 1, 
+    gridRows: {}, 
+    gridColumns: {},
+    rowEnd: [],
+    rowQ: 0,
+    colEnd: [],
+    colQ: 0
+  }
   arr = [1,2,3,4,5,6,7,8,9,10,11,12]
 
+  componentDidMount = () => {
+    let gridRows = {};
+    let gridColumns = {};
+    this.arr.forEach( item => {
+      gridRows[item] = [item, item];
+      gridColumns[item] = [item, item];
+    })
+
+    const rowQ = this.arr.length / this.state.gridTemplateColumns.split(" ").length;
+    const colQ =  this.state.gridTemplateColumns.split(" ").length;
+    const rowEnd = [];
+    for (let i = 1; i <= rowQ; i ++ ) {
+      rowEnd.push(i)
+    }
+    const colEnd = [];
+    for (let i = 1; i <= colQ; i ++ ) {
+      colEnd.push(i)
+    }
+    this.setState({gridRows, gridColumns, rowEnd, colEnd, rowQ, colQ})
+  }
   changeCategory = e => {
     console.log(e.target)
     this.setState({category: e.target.id})
   }
+  changeGridRow = e => {
+    let obj = {...this.state.gridRows};
+    let index = Number(e.target.dataset.index);
+    let value = Number(e.target.value)
+    obj[this.state.activeGridItem][index] = value;
+
+    if (index === 0) {
+      let rowEnd = [];
+      for (let i = value; i <= this.state.rowQ; i ++ ) {
+        rowEnd.push(i)
+      }
+      this.setState({rowEnd}) //CAN I UNIFY THIS WITH SET STATE BELOW?
+    }
+    this.setState({gridRows: Object.assign({}, this.state.gridRows, obj)})
+  }
+  changeGridColumn = e => {
+    let obj = {...this.state.gridColumns};
+    let index = Number(e.target.dataset.index);
+    let value = Number(e.target.value)
+    obj[this.state.activeGridItem][index] = value;
+
+    if (index === 0) {
+      let colEnd = [];
+      for (let i = value; i <= this.state.colQ; i ++ ) {
+        colEnd.push(i)
+      }
+      this.setState({colEnd}) //CAN I UNIFY THIS WITH SET STATE BELOW?
+    }
+    this.setState({gridColumns: Object.assign({}, this.state.gridColumns, obj)})
+  }
+
 
   render() {
     const {gridTemplateRows, gridTemplateColumns} = this.state;
     const parentStyle = {gridTemplateRows, gridTemplateColumns }
-    let options = [];
+   console.log(this.state.colEnd)
 
     return (
       <div>
@@ -22,7 +84,7 @@ export default class Grid extends Component {
           <Form.Group>
             <Form.Check type="radio" name="category" label="grid container" id="gridContainer" onChange={this.changeCategory} />
            
-            { this.state.category == "gridContainer" &&  
+            { this.state.category === "gridContainer" &&  
             <div>
               <InputGroup className="mb-3">
             <InputGroup.Prepend>
@@ -47,18 +109,78 @@ export default class Grid extends Component {
            
           <Form.Group>
             <Form.Check type="radio"  name="category" label="grid items" id="gridItems" onChange={this.changeCategory} />
-            { this.state.category === "gridItems" && 
-            <div>
-             <Form.Label>choose which grid item to edit:</Form.Label>
-              <Form.Control as="select" onChange={this.flexDirection}>
-              {
-                this.arr.map( (item, index) => {
-                  return <option value={item} key={index}>{item}</option>
-                })
-              }
-            </Form.Control>
-            </div>
-            }
+            <Table>
+              <thead>
+                <tr>
+                <th>grid item</th>
+                <th>css property</th>
+                <th>start</th>
+                <th>end</th>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td rowSpan="2">
+                    <Form.Control as="select">
+                        {
+                              this.arr.map( (item, index) => {
+                                  return <option value={item} key={index}>{item}</option>
+                                })
+                        }
+                    </Form.Control>
+                    </td>
+                    <td>
+                      grid-row
+                    </td>
+                    <td>
+                    <Form.Control as="select" onChange={this.changeGridRow} data-index="0">
+                        {
+                          [...new Array(this.state.rowQ).keys()].map( (item, index) => {
+                                  return <option value={item + 1} key={index}>{item + 1}</option>
+                                })
+                        }
+                    </Form.Control>
+                    </td>
+                    <td>
+                    <Form.Control as="select" onChange={this.changeGridRow} data-index="1">
+                        { 
+                              this.state.rowEnd.map( (item, index) => {
+                                  return <option value={item} key={index}>{item}</option>
+                                })
+                        }
+                    </Form.Control>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      grid-column
+                    </td>
+                    <td>
+                    <Form.Control as="select" onChange={this.changeGridColumn} data-index="0">
+                    {
+                          [...new Array(this.state.colQ).keys()].map( (item, index) => {
+                                  return <option value={item + 1} key={index}>{item + 1}</option>
+                                })
+                        }
+                    </Form.Control>
+                    </td>
+                    <td>
+                    <Form.Control as="select" onChange={this.changeGridColumn} data-index="1">
+                    { 
+                              this.state.colEnd.map( (item, index) => {
+                                  return <option value={item} key={index}>{item}</option>
+                                })
+                        }
+                    </Form.Control>
+                    </td>
+                  </tr>
+                </tbody>
+            </Table>
+           
+               
+           
+         
+          
           </Form.Group>
         </Form>
 
