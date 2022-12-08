61ba153622cf57377bb132d0a59905504b15e66b
ted <ted@warpedpuppy.com>
2022-12-08 08:18:19 -0500

    fixed dbs

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -33,7 +33,7 @@ class Menu extends Component {
             <div className="nav-links">
                 <div onClick={this.clickHandler}>css</div> 
                 <div onClick={this.clickHandler}>js</div>
-                {/* <div onClick={this.clickHandler}>dbs</div> */}
+                <div onClick={this.clickHandler}>dbs</div>
                 {/* <div onClick={this.clickHandler}>games</div> */}
                 {/* <div >art</div> */}
                 <div onClick={this.clickHandler}>about</div>
