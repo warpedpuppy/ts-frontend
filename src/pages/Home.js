dbe7cde30249c76ca84a6435be7ececfc9764a2a
ted <ted@warpedpuppy.com>
2022-12-08 12:44:44 -0500

    re-directed db call

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,7 +29,7 @@ export default class Home extends Component {
       this.setState({postresql:true})
     }
     result = await AWS.getTotalRecords();
-	console.log('result', RESULT)
+	console.log('result', result)
 
     if (result) {
       this.setState({aws:true})
